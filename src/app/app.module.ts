ecca21bda12e940fd51258184f6e03b8579df45d
STUNC <tunc0687@hotmail.com>
2020-05-22 22:39:42 +0300

    1-CLI ile yeni component oluşturuldu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { MoviesComponent } from './movies/movies.component';
+import { MovieComponent } from './movie/movie.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MoviesComponent,
+    MovieComponent
   ],
   imports: [
     BrowserModule,
